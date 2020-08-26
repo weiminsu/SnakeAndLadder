175f186487c1794555d0333dc2e141d71f9fdc74
giangnguyen <riverdang96@gmail.com>
2020-08-27 00:39:53 +1000

    Added tests for Snake SnakeController and Ladder

---
+++ test/au/edu/rmit/isys1117/group9/controller/SnakeControllerTest.java
@@ -0,0 +1,34 @@
+package au.edu.rmit.isys1117.group9.controller;
+
+import au.edu.rmit.isys1117.group9.model.Board;
+import au.edu.rmit.isys1117.group9.model.Dice;
+import au.edu.rmit.isys1117.group9.model.Snake;
+import au.edu.rmit.isys1117.group9.model.Trap;
+
+public class SnakeControllerTest {
+    @Before
+    public void setUp() {
+        board = Mockito.mock(Board.class);
+        controller = new SnakeController(board);
+
+    }
+    @Test
+    public void testSnakeMoveSuccess() {
+        Assert.assertNotNull(board);
+        doNothing().when(board).add(any(Snake.class));
+        Assert.assertTrue(controller.move());
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testSnakeMoveFailture() {
+
+        Assert.assertNotNull(board);
+        doThrow(new RuntimeException()).when(board).add(any(Snake.class));
+        Assert.assertFalse(controller.move());
+
+    }
+
+
+}
+
