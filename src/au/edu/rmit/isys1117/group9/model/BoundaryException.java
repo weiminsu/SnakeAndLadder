0cc2570d43eb0d668bc8cede87d813b474f527ba
Kaige Wang <s3549383@student.rmit.edu.au>
2020-08-27 12:48:37 +1000

    update some changes

---
+++ src/au/edu/rmit/isys1117/group9/model/BoundaryException.java
@@ -0,0 +1,13 @@
+package au.edu.rmit.isys1117.group9.model;
+
+public class BoundaryException extends Exception {
+	public BoundaryException() {
+		super();
+	}
+	
+	public BoundaryException (String mesg)
+	{
+		super(mesg);
+	}
+
+}
---
+++ src/au/edu/rmit/isys1117/group9/model/LadderPlacementException.java
@@ -0,0 +1,14 @@
+package au.edu.rmit.isys1117.group9.model;
+
+public class LadderPlacementException extends Exception {
+	
+	public LadderPlacementException() {
+		super();
+	}
+	
+	public LadderPlacementException (String mesg)
+	{
+		super(mesg);
+	}
+
+}
